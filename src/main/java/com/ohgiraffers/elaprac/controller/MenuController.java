15d7283c69c4583aaeef5e0c83e5f30461a09c99
wolfohgiraffers <wolf.ohgiraffers@gmail.com>
2024-05-09 11:32:41 +0900

    push

--- src/main/java/com/ohgiraffers/elaprac/controller/MenuController.java
+++ src/main/java/com/ohgiraffers/elaprac/controller/MenuController.java
@@ -19,7 +19,7 @@ public class MenuController {
 
     @GetMapping("health")
     public String healthCheck() {
-        return "fine!@!?@";
+        return "fine!@!?@-env-1";
     }
 
     @GetMapping("menus/{menuCode}")
