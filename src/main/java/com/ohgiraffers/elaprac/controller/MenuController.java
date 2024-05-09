8e6c79e6ab2cfacdf9885e6d782b4383cd87bcc5
wolfohgiraffers <wolf.ohgiraffers@gmail.com>
2024-05-09 12:06:39 +0900

    env-1 환경(green 배포)

--- src/main/java/com/ohgiraffers/elaprac/controller/MenuController.java
+++ src/main/java/com/ohgiraffers/elaprac/controller/MenuController.java
@@ -19,7 +19,7 @@ public class MenuController {
 
     @GetMapping("health")
     public String healthCheck() {
-        return "fine!@!?@-env-1";
+        return "fine!@!?@-en1";
     }
 
     @GetMapping("menus/{menuCode}")
