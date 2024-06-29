6d98824e00d647693a3f2db52ccc7c0661d2944d
wolfohgiraffers <wolf.ohgiraffers@gmail.com>
2024-06-29 16:39:59 +0900

    push

--- src/main/java/com/ohgiraffers/elaprac/controller/MenuController.java
+++ src/main/java/com/ohgiraffers/elaprac/controller/MenuController.java
@@ -19,7 +19,7 @@ public class MenuController {
 
     @GetMapping("health")
     public String healthCheck() {
-        return "home ela-prac is fine!! application.yml만 사용 after!! 5/21";
+        return "home ela-prac is fine!! application.yml만 사용 after!! 6/29";
     }
 
     @GetMapping("menus/{menuCode}")
